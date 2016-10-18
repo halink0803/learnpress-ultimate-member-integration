3c3331c15ba5a4131901b337509307a097f8b700
halink0803 <halink0803@gmail.com>
2016-10-18 17:09:42 +0700

    update

--- includes/profile-functions.php
+++ includes/profile-functions.php
@@ -3,7 +3,7 @@
  * @Author: halink
  * @Date:   2016-10-18 10:42:54
  * @Last Modified by:   halink
- * @Last Modified time: 2016-10-18 11:41:31
+ * @Last Modified time: 2016-10-18 15:16:44
  */
 
 /* add a custom tab to show user pages */
@@ -23,6 +23,12 @@ add_action('um_profile_content_pages_default', 'um_profile_content_pages_default
 function um_profile_content_pages_default( $args ) {
 	$user_id = get_current_user_id();
 	$courses = learn_press_get_enrolled_courses($user_id);
-	
-	print_r($courses);
+	// print_r($courses);
+	foreach($courses as $course):
+		?>
+			<div>
+				<a href="<?php echo $course->guid ?>"><?php echo $course->post_title ?></a>
+			</div>
+		<?php	
+	endforeach;
 }
